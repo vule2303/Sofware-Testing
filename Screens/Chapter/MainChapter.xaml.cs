b95d38f9bdab5491e34d072cdfd4a4cef49946a3
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-01-03 22:34:18 +0700

    Design Chapter Screen #2

--- Screens/Chapter/MainChapter.xaml.cs
+++ Screens/Chapter/MainChapter.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Microsoft.EntityFrameworkCore;
 using TestBuilder.Data;
 using TestBuilder.Models;
 
@@ -11,11 +12,36 @@ namespace TestBuilder.Screens.Chapter
     {
         private readonly TestDbContext _context = new();
         private readonly List<Subject> _subjects;
+        private readonly List<Items> _items;
+        public partial class Items
+        {
+            public required string NameChapter { get; set; }
+            public required string NameSubject { get; set; }
+        }
+
+        private void GetItems()
+        {
+            var items = _context.Chapters!.Include(chapters => chapters.Subject).ToList();
+            foreach (var item in items)
+            {
+                _items.Add(new Items
+                {
+                    NameChapter = item.Name,
+                    NameSubject = item.Subject.Name
+                });
+            }
+        }
+        
         public MainChapter()
         {
             InitializeComponent();
+            _subjects = new List<Subject>();
             _subjects = _context.Subjects!.ToList();
             SelectedSubject.ItemsSource = _subjects;
+            
+            _items = new List<Items>(); 
+            GetItems();
+            GridItems.ItemsSource = _items;
         }
         
         private void Button_Create(object sender, RoutedEventArgs e)
@@ -27,6 +53,7 @@ namespace TestBuilder.Screens.Chapter
                 Subject = _subjects[SelectedSubject.SelectedIndex],
             };
             _context.Chapters!.Add(chapter);
+            _context.SaveChanges();
         }
     }
 }
