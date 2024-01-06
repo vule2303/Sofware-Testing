6c159d15d8961a1630814e6d353c37858f8872f1
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-01-06 17:32:29 +0700

    Design Exam Screen #2

--- Screens/Exam/ManageExam.xaml.cs
+++ Screens/Exam/ManageExam.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
+﻿using System.Windows;
 using TestBuilder.Data;
 using TestBuilder.Models;
 
@@ -21,53 +7,73 @@ namespace TestBuilder.Screens.Exam
     /// <summary>
     /// Interaction logic for ManageExam.xaml
     /// </summary>
-    public partial class ManageExam : Window
+    public partial class ManageExam
     {
         private readonly TestDbContext _context = new ();
-        private List<Models.Subject> _subjects;
-        private List<Items> _items = new ();
+        private readonly List<Items> _items;
 
-        private class Items
+        private  class Items
         {
-            public int ExamId { get; set; }
-            public string ExamTitle { get; set; }
-            public int SubjectId { get; set; }
-            public string SubjectName { get; set; }
-            public int Amount { get; set; }
+            public int ExamId { get; init; }
+            public  required string ExamTitle {  set; get; }
+            public  int Amount { set; get; }
         }
         
         public ManageExam()
         {
             InitializeComponent();
+            _items = new List<Items>();
             InitData();
-            _subjects = new List<Models.Subject>();
-            _subjects = _context.Subjects.ToList();
-            ListSubject.ItemsSource = _subjects;
             LoadGrid();
-            
+            DataContext = this;
         }
         
         private void Insert(object sender, RoutedEventArgs e)
         {
-            _context.Exams.Add(new Models.Exam(){Title = TitleExam.Text});
+            _context.Exams.Add(new Models.Exam{Title = TitleExam.Text});
             _context.SaveChanges();
-            
+            LoadGrid();
+        }
+        
+        private async void Delete(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var item = GridItems.SelectedItem as Items;
+                var testExam = _context.TestExams.Where(exam => item != null && exam.ExamId == item.ExamId).ToList();
+                _context.TestExams.RemoveRange(testExam);
+
+                var examSubject = _context.ExamsSubjects.Where(exam => item != null && exam.ExamId == item.ExamId)
+                    .ToList();
+                _context.ExamsSubjects.RemoveRange(examSubject);
+
+                if (item != null)
+                {
+                    var exam = _context.Exams.FindAsync(item.ExamId);
+                    if (exam.Result != null) _context.Exams.Remove(exam.Result);
+                }
+
+                await _context.SaveChangesAsync();
+                LoadGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
 
         private void LoadGrid()
         {
-            var listItem = _context.ExamsSubjects
-                
-                .ToList();
+            var listItem = _context.Exams.ToList();
             foreach (var item in listItem)
             {
+                var count = _context.TestExams.Count(exam => exam.ExamId == item.ExamId);
                 _items.Add(new Items()
                 {
                     ExamId = item.ExamId,
-                    ExamTitle = item.Exam.Title,
-                    SubjectId = item.SubjectId,
-                    SubjectName = item.Subject.Name,
+                    ExamTitle = item.Title,
+                    Amount = count
                     });                
             }
             GridItems.ItemsSource = _items;
@@ -79,12 +85,12 @@ namespace TestBuilder.Screens.Exam
             _context.Subjects.Add(new Models.Subject(){Name = "Anh"});
             _context.Exams.Add(new Models.Exam(){Title = "Thi học kì 1"});
             _context.Exams.Add(new Models.Exam(){Title = "Thi học kì 2"});
-            _context.ExamsSubjects.Add(new Models.ExamsSubjects(){ExamId = 1, SubjectId = 1});
-            _context.ExamsSubjects.Add(new Models.ExamsSubjects(){ExamId = 1, SubjectId = 2});
-            _context.ExamsSubjects.Add(new Models.ExamsSubjects(){ExamId = 1, SubjectId = 3});
-            _context.ExamsSubjects.Add(new Models.ExamsSubjects(){ExamId = 2, SubjectId = 1});
-            _context.ExamsSubjects.Add(new Models.ExamsSubjects(){ExamId = 2, SubjectId = 2});
-            _context.ExamsSubjects.Add(new Models.ExamsSubjects(){ExamId = 2, SubjectId = 3});
+            _context.ExamsSubjects.Add(new ExamsSubjects(){ExamId = 1, SubjectId = 1});
+            _context.ExamsSubjects.Add(new ExamsSubjects(){ExamId = 1, SubjectId = 2});
+            _context.ExamsSubjects.Add(new ExamsSubjects(){ExamId = 1, SubjectId = 3});
+            _context.ExamsSubjects.Add(new ExamsSubjects(){ExamId = 2, SubjectId = 1});
+            _context.ExamsSubjects.Add(new ExamsSubjects(){ExamId = 2, SubjectId = 2});
+            _context.ExamsSubjects.Add(new ExamsSubjects(){ExamId = 2, SubjectId = 3});
             _context.Tests.Add(new Models.Test(){ Title = "Test 1", SubjectId = 1});
             _context.Tests.Add(new Models.Test(){ Title = "Test 2" , SubjectId = 2});
             _context.TestExams.Add(new TestExams(){TestId = 1, ExamId = 1});
