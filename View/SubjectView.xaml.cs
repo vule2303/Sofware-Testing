9dafbb80c2aaeb1ebcdb95c12f2bd129c1d5d854
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-01-07 03:50:28 +0700

    Add function: Create, Remove Subject; Until Update Subject

--- View/SubjectView.xaml.cs
+++ View/SubjectView.xaml.cs
@@ -1,69 +1,93 @@
-using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
+using TestBuilder.Data;
+using TestBuilder.Models;
 
 namespace TestBuilder.View;
 
-public partial class SubjectView : UserControl
+public partial class SubjectView
 {
-
+    private readonly TestDbContext _context = new();
+    private  List<Subject>? _subjects;
     public SubjectView()
     {
         InitializeComponent();
-         var subjects = new ObservableCollection<TestBuilder.Models.Subject>
-                 {
-                     new()
-                     {
-                         SubjectId = 1200068,
-                         Name = "Toán Cao Cấp",
-                         Chapters = [],
-                         ExamsSubjects = []
-                     },
-                     new()
-                     {
-                         SubjectId = 1200668,
-                         Name = "Lập Trình Window",
-                         Chapters = [],
-                         ExamsSubjects = []
-                     },
-                     new()
-                     {
-                         SubjectId = 1232068,
-                         Name = "Lập Trình Android",
-                         Chapters = [],
-                         ExamsSubjects = []
-                     },
-                     new()
-                     {
-                         SubjectId = 1340068,
-                         Name = "Lập Trình Web",
-                         Chapters = [],
-                         ExamsSubjects = []
-                     },
-                     new()
-                     {
-                         SubjectId = 1202368,
-                         Name = "Quản Lý Dự Án",
-                         Chapters = [],
-                         ExamsSubjects = []
-                     },
-                     new()
-                     {
-                         SubjectId = 1234305,
-                         Name = "Phát Triển Website",
-                         Chapters = [],
-                         ExamsSubjects = []
-                     },
-                     new()
-                     {
-                         SubjectId = 1460068,
-                         Name = "Công Nghệ Phần Mềm",
-                         Chapters = [],
-                         ExamsSubjects = []
-                     }
-                 };
-                    subjectDataGrid.ItemsSource = subjects;
+        InitData();
+        LoadData();
     }
-  
+    
+    
+    private void AddSubject(object sender, RoutedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(TxtAddSubject.Text))
+        {
+            _context.Subjects.Add(new Subject(){Name = TxtAddSubject.Text});
+            _context.SaveChanges();
+            LoadData();
+            TxtAddSubject.Text="";
+        }
+        else
+            MessageBox.Show("Vui lòng nhập tên môn học");
+    }
+    private void EditClick(object sender, RoutedEventArgs e)
+    {
+        AddSubjectButton.Visibility = Visibility.Hidden;
+        UpdateSubjectButton.Visibility = Visibility.Visible;
+        VisibleAddButton.Visibility = Visibility.Visible;
+    }
+    
+    private void RemoveClick(object sender, RoutedEventArgs e)
+    {
+        var item = (SubjectDataGrid.SelectedItem as Subject)?.SubjectId;
+        var subject = _context.Subjects.Find(item);
+        if (subject != null)
+        {
+            _context.Subjects.Remove(subject);
+            _context.SaveChanges();
+            LoadData();
+        }
+    }
+
+    private void LoadData()
+    {
+        _subjects = new List<Subject>();
+        _subjects = _context.Subjects.ToList();
+        SubjectDataGrid.ItemsSource = _subjects;
+
+    }
+    private void InitData()
+    {
+        _context.Add(new Subject(){Name = "Toán Cao Cấp"});
+        _context.Add(new Subject(){Name = "Lập Trình Window"});
+        _context.Add(new Subject(){Name = "Lập Trình Android"});
+        _context.Add(new Subject(){Name = "Lập Trình Web"});
+        _context.SaveChanges();
+    }
+
+    private void UpdateSubject(object sender, RoutedEventArgs e)
+    {
+        var item = SubjectDataGrid.ItemsSource as Subject;
+        var subject = _context.Subjects.Find(item.SubjectId);
+        MessageBox.Show(item.Name);
+        TxtAddSubject.Text = item.Name;
+        if (subject != null)
+        {
+            if (!string.IsNullOrEmpty(TxtAddSubject.Text))
+            {
+                subject.Name = TxtAddSubject.Text;
+                _context.SaveChanges();
+                LoadData();
+                TxtAddSubject.Text = "";
+                ClickVisible(sender, e);
+            }
+            else
+                MessageBox.Show("Vui lòng điền tên môn học");
+        }
+    }
+
+    private void ClickVisible(object sender, RoutedEventArgs e)
+    {
+        AddSubjectButton.Visibility = Visibility.Visible;
+        UpdateSubjectButton.Visibility = Visibility.Hidden;
+        VisibleAddButton.Visibility = Visibility.Hidden;
+    }
 }
