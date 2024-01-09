e3e25929b835f4d5364c20d782952b2036daca3f
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-01-09 09:51:12 +0700

    Done Exam Design

--- Screens/Exam/ExamView.xaml.cs
+++ Screens/Exam/ExamView.xaml.cs
@@ -1,13 +1,21 @@
 using System.Windows;
-using System.Windows.Controls;
+using MahApps.Metro.IconPacks;
+using Microsoft.EntityFrameworkCore;
 using TestBuilder.Data;
 
 namespace TestBuilder.Screens.Exam;
 
-public partial class ExamView : UserControl
+public partial class ExamView
 {
     private readonly TestDbContext _context = new ();
     private List<Items>? _items;
+    private List<GridData>? _datas;
+
+    public class GridData
+    {
+        public  string? SubjectName {  set; get; }
+        public  int Amount { set; get; }
+    }
 
     public class Items
     {
@@ -36,51 +44,39 @@ public partial class ExamView : UserControl
         
         private void View(object sender, RoutedEventArgs e)
         {
-            /*var item = GridItems.SelectedItem as Items;
-            Window examSubject = new ExamSubject(item, _context);
-            examSubject.Show();*/
+            GridItems2.Visibility = Visibility.Visible;
+            GridItems.Visibility = Visibility.Hidden;
+            ButtonRollback.Visibility = Visibility.Visible;
+            Group.Visibility = Visibility.Hidden;
+            LoadGrid();
         }
         
-        private void Update(object sender, RoutedEventArgs e)
+        private void EditClick(object sender, RoutedEventArgs e)
         {
-            TitleExam.Visibility = Visibility.Visible;
-            /*ButtonUpdate.Visibility = Visibility.Visible;
-            AddButton.Visibility = Visibility.Visible;
-            Default.Visibility = Visibility.Hidden;*/
-            TitleExam.Visibility = Visibility.Hidden;
-            TitleExam.Text = (GridItems.SelectedItem as Items)?.ExamTitle ?? string.Empty;
+            var item = (GridItems.SelectedItem as Items)?.ExamTitle;
+            if (item != null) TitleExam.Text = item;
+            ButtonAction.Click -= Insert;
+            ButtonAction.Click += Update;
+            IconAction.Kind = PackIconMaterialKind.PencilOutline;
+            ButtonName.Text = "Cập nhật";
+            VisibleAddButton.Visibility = Visibility.Visible;
         }
 
-        private void Add(object sender, RoutedEventArgs e)
+        private void Update(object sender, RoutedEventArgs e)
         {
-            TitleExam.Visibility = Visibility.Hidden;
-            /*ButtonUpdate.Visibility = Visibility.Hidden;
-            AddButton.Visibility = Visibility.Hidden;
-            Default.Visibility = Visibility.Visible;*/
-            TitleExam.Visibility = Visibility.Visible;
+            var item = (GridItems.SelectedItem as Items)?.ExamId;
+            var exam = _context.Exams.Find(item);
+            if (exam == null) return;
+            exam.Title = TitleExam.Text;
+            _context.SaveChanges();
+            LoadGrid();
+            TitleExam.Text = "";
+            ButtonAction.Click -= Update;
+            ButtonAction.Click += Insert;
+            IconAction.Kind = PackIconMaterialKind.Plus;
+            ButtonName.Text = "Thêm kì thi";
+            VisibleAddButton.Visibility = Visibility.Hidden;
         }
-        
-        private void UpdateTitle(object sender, RoutedEventArgs e)
-        {
-            if (!String.IsNullOrEmpty(TitleExam.Text))
-            {
-                var item = (GridItems.SelectedItem as Items)?.ExamId;
-                var update = _context.Exams.Find(item);
-                if (update != null)
-                    update.Title = TitleExam.Text;
-                _context.SaveChanges();
-                LoadGrid();
-                TitleExam.Text = "";
-                TitleExam.Visibility = Visibility.Hidden;
-                /*ButtonUpdate.Visibility = Visibility.Hidden;
-                AddButton.Visibility = Visibility.Hidden;
-                Default.Visibility = Visibility.Visible;*/
-                TitleExam.Visibility = Visibility.Visible;
-            }
-            else
-                MessageBox.Show("Vui lòng nhập tên kì thi");
-        }
-        
         private async void Delete(object sender, RoutedEventArgs e)
         {
             try
@@ -103,18 +99,63 @@ public partial class ExamView : UserControl
 
         private void LoadGrid()
         {
-            _items = new List<Items>();
-            var listItem = _context.Exams.ToList();
-            foreach (var item in listItem)
+            if (GridItems.Visibility == Visibility.Visible)
             {
-                var count = _context.TestExams.Count(exam => exam.ExamId == item.ExamId);
-                _items.Add(new Items()
+                _items = new List<Items>();
+                var listItem = _context.Exams.ToList();
+                foreach (var item in listItem)
                 {
-                    ExamId = item.ExamId,
-                    ExamTitle = item.Title,
-                    Amount = count
+                    var count = _context.TestExams.Count(exam => exam.ExamId == item.ExamId);
+                    _items.Add(new Items()
+                    {
+                        ExamId = item.ExamId,
+                        ExamTitle = item.Title,
+                        Amount = count
                     });                
+                }
+                GridItems.ItemsSource = _items;
             }
-            GridItems.ItemsSource = _items;
+            if (GridItems2.Visibility == Visibility.Visible)
+            {
+                var exam = (GridItems.SelectedItem as Items)?.ExamId;
+                _datas = new List<GridData>();
+                var listItem = _context.ExamsSubjects.Where(x => exam != null && x.ExamId == exam)
+                    .Include(examsSubjects => examsSubjects.Subject).ToList();
+                foreach (var item in listItem)
+                {
+                    var count = _context.Tests
+                        .Include(s => s.Subject)
+                        .Include(te => te.TestExams)
+                        .Count(x => exam != null 
+                                    && x.TestExams != null 
+                                    && x.SubjectId == item.SubjectId 
+                                    && exam == x.TestExams.Select(te => te.ExamId)
+                                        .FirstOrDefault());
+                    _datas.Add(new GridData()
+                    {
+                        SubjectName = item.Subject?.Name,
+                        Amount = count
+                    });
+                }
+                GridItems2.ItemsSource = _datas;
+            }
+        }
+
+        private void ClickVisible(object sender, RoutedEventArgs e)
+        {
+            ButtonAction.Click -= Update;
+            ButtonAction.Click += Insert;
+            IconAction.Kind = PackIconMaterialKind.Plus;
+            ButtonName.Text = "Thêm kì thi";
+            VisibleAddButton.Visibility = Visibility.Hidden;
+        }
+
+        private void ClickUndo(object sender, RoutedEventArgs e)
+        {
+            ButtonRollback.Visibility = Visibility.Hidden;
+            Group.Visibility = Visibility.Visible;
+            GridItems.Visibility = Visibility.Visible;
+            GridItems2.Visibility = Visibility.Hidden;
+            LoadGrid();
         }
 }
